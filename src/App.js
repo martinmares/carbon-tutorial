3c2ed1b7e2901711022606e4fab94b5315e061d8
Martin Mareš <mares@datalite.cz>
2020-04-15 11:38:52 +0200

    Add ReactRouter and REPO PAGE and LANDING PAGE

--- src/App.js
+++ src/App.js
@@ -5,13 +5,21 @@ import { Button } from 'carbon-components-react';
 import { Content } from 'carbon-components-react/es/components/UIShell';
 import TutorialHeader from './components/TutorialHeader';
 
+// support for router
+import { Route, Switch } from 'react-router-dom';
+import LandingPage from './content/LandingPage';
+import RepoPage from './content/RepoPage';
+
 class App extends Component {
   render() {
     return (
       <>
         <TutorialHeader />
         <Content>
-          <Button>Button</Button>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/repos" component={RepoPage} />
+          </Switch>
         </Content>
       </>
     );
